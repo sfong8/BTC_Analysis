56c92f892e5c2f3988dafd4c64e422fe96bc5225
Simon Fong <simonfong93@gmail.com>
2021-01-20 20:43:26 +0000

    save file to exploration

--- src/Exploration.py
+++ src/Exploration.py
@@ -1,41 +1,8 @@
 import pandas as pd
-
-
-df = pd.read_parquet(r'../Data/cleaned_dataset_20210117.parquet')
-
-###create a new column that is the date column -7
-# df = df.head(n=100)
-
 from datetime import timedelta
 
-df['NEW_DATETIME'] = df['DATETIME_CONVERTED'].apply(lambda x :x -timedelta(days=1))
-
-##read in the tweets (grouped)
-
-tweets_group = pd.read_parquet(r'../Data/Tweets/tweets_cleaned.parquet')
-df['DATETIME_CONVERTED'] = pd.to_datetime(df.DATETIME_CONVERTED, utc = True)
-
-df=df.merge(tweets_group,how='left',on='DATETIME_CONVERTED')
-df['TWEET_COUNT'].fillna(0,inplace=True)
-df.columns = ['DATETIME_CONVERTED', 'Volume_(BTC)', 'y', 'ds',
-       'TWEET_COUNT']
-###split the data 20-80%
-###number of rows
-rows = int(df.shape[0]*0.8)
-
-train_df = df[:rows]
-test_df = df[rows:]
-
-
-from fbprophet import Prophet
-df_prophet = Prophet(changepoint_prior_scale=0.15, daily_seasonality=True)
-df_prophet.fit(train_df)
-
-fcast_time=144   # 1 year
 from datetime import datetime
-print(datetime.now())
-df_forecast = df_prophet.make_future_dataframe(periods= fcast_time, freq='10min')
-df_forecast.tail(10)
-print(datetime.now())
+from fbprophet import Prophet
 
-df_forecast = df_prophet.predict(df_forecast)
+df_forecast=pd.read_parquet('../Data/df_forcast.parquet')
+
